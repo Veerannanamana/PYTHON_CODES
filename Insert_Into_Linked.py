b3ef998c6e40b9f9b3e80176b10e50d6856f5bfd
NAMANA VEERA NAGA MAHA LAKSHMAN <143400275+Veerannanamana@users.noreply.github.com>
2025-08-30 21:56:01 +0530

    Insert_Into_Linked.py

---
+++ Insert_Into_Linked.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class Linked:
+    def __init__(self):
+        self.head=None
+    def insert_begin(self,data):
+        new_node=Node(data)
+        new_node.next=self.head
+        self.head=new_node
+    def insert_end(self,data):
+        new_node=Node(data)
+        if self.head is None:
+            new_node.next=self.head
+            self.head=new_node
+            return
+        temp=self.head
+        while temp.next:
+            temp=temp.next
+        temp.next=new_node
+    def insert_pos(self,data,pos):
+        new_node=Node(data)
+        if pos==0:
+            new_node.next=self.head
+            self.head=new_node
+            return
+        temp=self.head
+        for i in range(pos-1):
+            if temp is None:
+                print("Position Out of range")
+                return
+            temp=temp.next
+        if temp is None:
+            print("Position Out of range")
+            return
+        new_node.next=temp.next
+        temp.next=new_node
+    def display(self):
+        temp=self.head
+        while temp:
+            print(temp.data,end=" -> ")
+            temp=temp.next
+        print("end")
+l=Linked()
+l.insert_pos(5,0)
+l.insert_begin(20)
+l.insert_begin(10)
+l.insert_end(30)
+l.insert_end(40)
+l.insert_pos(5,5)
+l.display()
